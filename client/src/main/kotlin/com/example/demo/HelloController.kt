9c88915806d1adb5f638fce1d55079cfe8ed2f5e
Raed <raedbh@gmail.com>
2017-11-22 14:01:19 +0100

    Fix @RefreshScope emplacement

--- client/src/main/kotlin/com/example/demo/HelloController.kt
+++ client/src/main/kotlin/com/example/demo/HelloController.kt
@@ -1,9 +1,11 @@
 package com.example.demo
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RefreshScope
 @RestController
 class HelloController {
 
